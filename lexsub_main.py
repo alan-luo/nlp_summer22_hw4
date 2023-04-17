3a2385ed7751079b2b6645686bcd183d3af55c43
Alan Luo <me@alanluo.com>
2023-04-17 14:00:04 +0000

    completed part4

--- lexsub_main.py
+++ lexsub_main.py
@@ -30,7 +30,7 @@ def get_candidates(lemma, pos) -> List[str]:
     for synset in synsets:
         lemmas.update(synset.lemmas())
 
-    word_set = set(map(lambda l: l.name(), lemmas))
+    word_set = set(map(lambda l: l.name().replace('_', ' '), lemmas))
     word_set.remove(lemma)
 
     return list(word_set)
@@ -68,7 +68,7 @@ def wn_frequency_predictor(context : Context) -> str:
             max_lem = lem
 
 
-    return max_lem
+    return max_lem.replace('_', ' ')
 
 # Get the definition and examples of a synset, remove all stopwords, concat into a big array 
 def get_syn_gloss(synset, remove_stops): # str[]
@@ -116,7 +116,7 @@ def wn_simple_lesk_predictor(context : Context) -> str:
             best_synsets.append(synset)
 
     max_syn_count = -1
-    best_syn_lemma = None
+    best_syn_lemma = None # basically best_best_lemma lol
 
     # Pick the synset->lemma pair with the highest freq
     for synset in best_synsets:
@@ -127,14 +127,14 @@ def wn_simple_lesk_predictor(context : Context) -> str:
             if lemma.count() > max_count:
                 max_count = lemma.count()
                 best_lemma = lemma
-        
+
         # now, compare with our global best
         if max_count > max_syn_count:
             max_syn_count = max_count
             best_syn_lemma = best_lemma
 
-    return best_syn_lemma.name()
-   
+    return best_syn_lemma.name().replace('_', ' ')
+
 
 class Word2VecSubst(object):
         
@@ -142,7 +142,23 @@ class Word2VecSubst(object):
         self.model = gensim.models.KeyedVectors.load_word2vec_format(filename, binary=True)    
 
     def predict_nearest(self,context : Context) -> str:
-        return None # replace for part 4
+        candidates = get_candidates(context.lemma, context.pos)
+
+        max_sim = -1
+        best_lem = None
+        for candidate in candidates:
+            try:
+                sim = self.model.similarity(context.lemma, candidate)
+                if (sim > max_sim):
+                    max_sim = sim
+                    best_lem = candidate
+            except KeyError:
+                continue
+
+        if best_lem == None:
+            return wn_frequency_predictor(context)
+
+        return best_lem
 
 
 class BertPredictor(object):
@@ -160,10 +176,10 @@ if __name__=="__main__":
 
     # At submission time, this program should run your best predictor (part 6).
 
-    #W2VMODEL_FILENAME = 'GoogleNews-vectors-negative300.bin.gz'
-    #predictor = Word2VecSubst(W2VMODEL_FILENAME)
+    W2VMODEL_FILENAME = '~/GoogleNews-vectors-negative300.bin.gz'
+    predictor = Word2VecSubst(W2VMODEL_FILENAME)
 
     for context in read_lexsub_xml(sys.argv[1]):
         # print(context)  # useful for debugging
-        prediction = wn_simple_lesk_predictor(context) 
+        prediction = predictor.predict_nearest(context) 
         print("{}.{} {} :: {}".format(context.lemma, context.pos, context.cid, prediction))
